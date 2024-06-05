a6a35cd5eaac7de00d87862272a85662cfa44a13
Ricardo <rjnv098@gmail.com>
2024-06-05 10:57:01 -0400

    base

---
+++ src/test/java/univsys/asistenciadocente/AsistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package univsys.asistenciadocente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
