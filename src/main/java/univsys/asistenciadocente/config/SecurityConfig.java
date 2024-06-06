239d9e85b1e8ab0aa9ed47955e47d2dc5eb136ff
Ricardo <rjnv098@gmail.com>
2024-06-05 22:44:43 -0400

    seguridad

--- src/main/java/univsys/asistenciadocente/config/SecurityConfig.java
+++ src/main/java/univsys/asistenciadocente/config/SecurityConfig.java
@@ -6,8 +6,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
--- src/main/java/univsys/asistenciadocente/controller/CustomerController.java
+++ src/main/java/univsys/asistenciadocente/controller/CustomerController.java
@@ -1,8 +1,5 @@
 package univsys.asistenciadocente.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
