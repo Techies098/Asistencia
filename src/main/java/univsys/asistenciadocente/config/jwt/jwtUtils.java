13c829e4c21368716c013f9576c991e7ec26d01d
Ricardo <rjnv098@gmail.com>
2024-06-06 23:04:10 -0400

    seguridad

--- src/main/java/univsys/asistenciadocente/config/jwt/jwtUtils.java
+++
@@ -1,9 +0,0 @@
-package univsys.asistenciadocente.config.jwt;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class jwtUtils {
-    private String SecretKey;
-    private String timeExpiration;
-}
