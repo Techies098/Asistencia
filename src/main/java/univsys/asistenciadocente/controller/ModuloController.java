298c8461c929fa587e97d840b89026f2b4fee2de
Ricardo <rjnv098@gmail.com>
2024-06-09 00:57:47 -0400

    store de modulos

--- src/main/java/univsys/asistenciadocente/controller/ModuloController.java
+++ src/main/java/univsys/asistenciadocente/controller/ModuloController.java
@@ -1,11 +1,12 @@
 package univsys.asistenciadocente.controller;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import univsys.asistenciadocente.models.CarreraEntity;
+import univsys.asistenciadocente.models.FacultadEntity;
 import univsys.asistenciadocente.models.ModuloEntity;
 import univsys.asistenciadocente.repositories.ModuloRepository;
 
@@ -30,4 +31,12 @@ public class ModuloController {
         response.put("data", modulos);
         return ResponseEntity.ok(response);
     }
+    @Transactional
+    @PostMapping("/store")
+    public ResponseEntity<ModuloEntity> crearModulo(@RequestBody ModuloEntity modulo) {
+        ModuloEntity nuevoModulo = moduloRepository.save(modulo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoModulo);
+    }/*{
+    "numero": 555
+    }*/
 }
