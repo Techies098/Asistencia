d13867b0b27e3c9d0db2de818cbcf17cba3c7941
Ricardo <rjnv098@gmail.com>
2024-06-18 00:01:00 -0400

    restaurar el seeder

--- src/main/java/univsys/asistenciadocente/seeders/DatabaseSeeder.java
+++ src/main/java/univsys/asistenciadocente/seeders/DatabaseSeeder.java
@@ -152,7 +152,7 @@ public class DatabaseSeeder {
         asignargrupo(10L,2646L);
 
     }
-@Transactional
+    @Transactional
     public void asignargrupo(Long idgrupo, Long idhorario) {
         HorarioEntity horario = horarioRepository.findById(idhorario).orElseThrow(() -> new RuntimeException("Horario not found"));
         GrupoEntity grupo = grupoRepository.findById(idgrupo).orElseThrow(() -> new RuntimeException("Grupo not found"));
@@ -208,6 +208,10 @@ public class DatabaseSeeder {
             horarioSeed(aulaId, "10:00", "10:45",dia);
             horarioSeed(aulaId, "10:45", "11:30",dia);
             horarioSeed(aulaId, "11:30", "12:15",dia);
+            horarioSeed(aulaId, "12:15", "13:00",dia);
+            horarioSeed(aulaId, "13:45", "14:30",dia);
+            horarioSeed(aulaId, "14:30", "15:15",dia);
+            horarioSeed(aulaId, "15:15", "16:00",dia);
         }
     }
 
@@ -226,8 +230,8 @@ public class DatabaseSeeder {
     }
 
     public void seedAulas() {
-        for (Long moduloId = 1L; moduloId <= 2L; moduloId++) {
-            for (int aulaNumero = 1; aulaNumero <= 10; aulaNumero++) {
+        for (Long moduloId = 1L; moduloId <= 3L; moduloId++) {
+            for (int aulaNumero = 1; aulaNumero <= 20; aulaNumero++) {
                 aulaSeed(aulaNumero, moduloId);
             }
         }
@@ -245,11 +249,17 @@ public class DatabaseSeeder {
     }
 
     public void CarreraMateriaSeeder() {
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < 17; i++) {
             Long materia = (long) i;
             carreraMateriaSeed(1 + i / 5, 1L, materia);
             carreraMateriaSeed(1 + i / 5, 2L, materia);
             carreraMateriaSeed(1 + i / 5, 3L, materia);
+        }
+        for (int i = 1; i < 11; i++) {
+            Long materia = (long) i + 16;
+            carreraMateriaSeed(1 + i / 5, 4L, materia);
+            carreraMateriaSeed(1 + i / 5, 5L, materia);
+            carreraMateriaSeed(1 + i / 5, 6L, materia);
         }
     }
 
@@ -392,4 +402,3 @@ public class DatabaseSeeder {
         }
     }
 }
-
